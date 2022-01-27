f66b1a63a2dabbeaaf3691047577609827c3408f
Cent222 <toscent.bpg@gmail.com>
2022-01-26 23:18:25 -0300

    renderiza chão e flappyBird

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Praticando criação de games:
 
 - Replicando o projeto do devsoutinho
+4
+## Links Úteis
+
+- **Canvas**
+  - Documentação
+    - getContext(): https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+    - drawImage(): https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
--- jogo.js
+++ jogo.js
@@ -1,4 +1,4 @@
-console.log('[DevSoutinho] Flappy Bird');
+console.log('[Cent222] Flappy Bird');
 
 const sprites = new Image();
 sprites.src = './sprites.png';
@@ -6,89 +6,54 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
-
-// [Plano de Fundo]
-const planoDeFundo = {
-  spriteX: 390,
-  spriteY: 0,
-  largura: 275,
-  altura: 204,
-  x: 0,
-  y: canvas.height - 204,
-  desenha() {
-    contexto.fillStyle = '#70c5ce';
-    contexto.fillRect(0,0, canvas.width, canvas.height)
-
-    contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      planoDeFundo.x, planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
-
-    contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
-  },
-};
-
-// [Chao]
 const chao = {
-  spriteX: 0,
+  spritesX: 0,
   spriteY: 610,
   largura: 224,
   altura: 112,
   x: 0,
   y: canvas.height - 112,
-  desenha() {
-    contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.largura, chao.altura,
-    );
 
+  desenha(){
     contexto.drawImage(
       sprites,
-      chao.spriteX, chao.spriteY,
+      chao.spritesX, chao.spriteY,
       chao.largura, chao.altura,
-      (chao.x + chao.largura), chao.y,
-      chao.largura, chao.altura,
+      chao.x , chao.y ,
+      chao.largura, chao.altura
     );
-  },
-};
-
-const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
-  desenha() {
     contexto.drawImage(
       sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
+      chao.spritesX, chao.spriteY,
+      chao.largura, chao.altura,
+      chao.x + chao.largura , chao.y ,
+      chao.largura, chao.altura
     );
   }
 }
 
+const flappyBird = {
+  spritesX: 0,
+  spriteY: 0,
+  largura: 34,
+  altura: 24,
+  x: 10,
+  y: 50,
+  desenha(){
+    contexto.drawImage(
+      sprites, //imagem
+      flappyBird.spritesX, flappyBird.spriteY, //posicao no sprite x e sprite y
+      flappyBird.largura, flappyBird.altura, //largura e altura para recorte no sprite
+      flappyBird.x, flappyBird.y, //posicao onde vai desenhar
+      flappyBird.largura, flappyBird.altura // tamanho que vai desenhar
+    )
+  
+  }
+}
+
 function loop() {
-  planoDeFundo.desenha();
   chao.desenha();
   flappyBird.desenha();
-
-  flappyBird.y = flappyBird.y + 1;
-
   requestAnimationFrame(loop);
 }
 
